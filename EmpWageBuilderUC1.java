8002f5b9372c2a7ea5ef7fd7e128beda6498ec9d
MasterX01 <aakashsaxena14@gmail.com>
2021-03-09 09:02:02 +0530

    [akash] ADD. Added UC-2 file in branch

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -1,13 +1,19 @@
 public class EmpWageBuilderUC1 {
 	public static void main(String[] args) {
 		//constants
-		int IS_FULL_TIME = 1;
+		static final int IS_FULL_TIME = 1;
+		static final int EMP_RATE_PER_HOUR = 20;
+		//variables
+		int empHrs = 0;
+		int empWage = 0;
 		//Computation
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if(empCheck == IS_FULL_TIME)
-			System.out.println("Employee is Present");
+			empHrs = 8;
 		else
-			System.out.println("Employee is Absent");
+			empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Employee Wage - " + empWage);
 	}
 }
 

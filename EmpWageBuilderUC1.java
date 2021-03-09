eb29c7da196bba2f05e68db34fc650e42623e1c3
MasterX01 <aakashsaxena14@gmail.com>
2021-03-09 10:06:29 +0530

    [akash] ADD. added UC-6 file in the branch

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -1,16 +1,21 @@
 public class EmpWageBuilderUC1 {
-	//This is UC-5
+	//This is UC-6
 
 	//constants
         public static final int IS_FULL_TIME = 1;
         public static final int IS_PART_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 10;
+
 	public static void main(String[] args) {
 		//variables
 		int empHrs = 0, empWage = 0, totalEmpWage = 0;
+		int totalEmpHrs = 0, totalWorkingDays = 0;
+
 		//Computation
-		for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+		while (totalWorkingDays < NUM_OF_WORKING_DAYS &&
+			totalEmpHrs <= MAX_HRS_IN_MONTH) {
 			int empCheck = (int)(Math.floor(Math.random() * 10) % 3);
 			switch (empCheck) {
 				case IS_FULL_TIME:
@@ -26,6 +31,8 @@ public class EmpWageBuilderUC1 {
 			empWage = empHrs * EMP_RATE_PER_HOUR;
 			totalEmpWage += empWage;
 			System.out.println("EmployeeWage - " + empWage);
+			totalWorkingDays++;
+			totalEmpHrs += empHrs;
 		}
 		System.out.println("Total Employee Wage - " + totalEmpWage);
 	}

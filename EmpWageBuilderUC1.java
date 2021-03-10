122e5744decb7aff9aff8fa31c54e8755d66c5e8
MasterX01 <aakashsaxena14@gmail.com>
2021-03-10 16:30:34 +0530

    [Akash] ADD. added UC-7 working code with OOPS concept

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -8,33 +8,52 @@ public class EmpWageBuilderUC1 {
 	public static final int NUM_OF_WORKING_DAYS = 20;
 	public static final int MAX_HRS_IN_MONTH = 100;
 
-	public static void main(String[] args) {
-		//variables
-		int empHrs = 0, empWage = 0, totalEmpWage = 0;
-		int totalEmpHrs = 0, totalWorkingDays = 0;
+	public int empCheck() {
 
-		//Computation
-		while (totalWorkingDays < NUM_OF_WORKING_DAYS &&
-			totalEmpHrs <= MAX_HRS_IN_MONTH) {
-			int empCheck = (int)(Math.floor(Math.random() * 10) % 3);
-			switch (empCheck) {
-				case IS_FULL_TIME:
-					empHrs = 8;
-					break;
-				case IS_PART_TIME:
-					empHrs = 4;
-					break;
-				default:
-					empHrs = 0;
-					break;
-			}
-			empWage = empHrs * EMP_RATE_PER_HOUR;
-			totalEmpWage += empWage;
-			System.out.println("EmployeeWage - " + empWage);
+		int empHrs = 0;
+		int empCheck = (int)(Math.floor(Math.random() * 10) % 3);
+                switch (empCheck) {
+			case IS_FULL_TIME:
+				empHrs = 8;
+                                break;
+                        case IS_PART_TIME:
+                                empHrs = 4;
+                                break;
+                        default:
+                                empHrs = 0;
+                                break;
+                }
+		return empHrs;
+
+	}
+
+	public int wageCalculation() {
+
+                int totalEmpHrs = 0, totalWorkingDays = 0, totalEmpWage = 0;
+
+                //Computation
+                while (totalWorkingDays < NUM_OF_WORKING_DAYS &&
+                        totalEmpHrs <= MAX_HRS_IN_MONTH) {
 			totalWorkingDays++;
-			totalEmpHrs += empHrs;
-		}
-		System.out.println("Total Employee Wage - " + totalEmpWage);
+                        int empHrs = empCheck();
+
+                        int empWage = empHrs * EMP_RATE_PER_HOUR;
+                        totalEmpWage += empWage;
+                        System.out.println("Employee Wage for day "+ totalWorkingDays + " is " + empWage);
+                        totalEmpHrs += empHrs;
+                }
+		return totalEmpWage;
+
+	}
+
+	public static void main(String[] args) {
+
+		EmpWageBuilderUC1 obj = new EmpWageBuilderUC1();
+
+		//variables
+		int totalWage = obj.wageCalculation();
+
+		System.out.println("Total Employee Wage - " + totalWage);
 	}
 }
 
